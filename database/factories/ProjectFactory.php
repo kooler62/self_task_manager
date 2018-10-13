6499a46171a345dca471b46adcdf3a5c77145d3f
kooler62 <kooler62@gmail.com>
2018-10-13 21:48:21 +0300

    fix projectFactory

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -6,7 +6,6 @@ use App\Entities\Project;
 $factory->define(Project::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence($nbWords = 4, $variableNbWords = true),
-        'description' => $faker->text(200),
         'user_id' => $faker->numberBetween(1, 10),
         'status' => $faker->randomElement(['opened', 'closed']),
     ];
