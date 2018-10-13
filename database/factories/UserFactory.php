cbb339ecce9a3f70f77e42db730e30635c4945a3
kooler62 <kooler62@gmail.com>
2018-10-13 22:09:05 +0300

    add avatar, points field to User

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -9,5 +9,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+        'points' => $faker->numberBetween(100, 1000),
+        'avatar' => $faker->imageUrl(200, 200),
     ];
 });
